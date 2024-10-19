b66d7118a284bc35eb3adeadc208767b6afc7283
alesande21 <via.rzv19@gmail.com>
2024-10-19 10:26:13 +0300

    upd 2.2

--- Level_2/L2.2/main.go
+++ Level_2/L2.2/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	ntp2 "github.com/beevik/ntp"
+)
 
+func main() {
+	time, err := ntp2.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(time)
 }
