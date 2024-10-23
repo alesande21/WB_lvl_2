8fc08da6f757cef91edc7a6a026bc116891e2a2c
alesande21 <via.rzv19@gmail.com>
2024-10-23 18:33:09 +0300

    upd 2.4 add test для флага n

--- Level_2/L2.4/sort_test.sh
+++ Level_2/L2.4/sort_test.sh
@@ -4,7 +4,7 @@ GREEN='\e[32m'
 NC='\e[0m'
 RED='\e[31m'
 
-arr=(-b)
+arr=(-n)
 
 printf "${GREEN}-----RUNNING TESTS-----${NC}\n"
 
@@ -12,7 +12,7 @@ printf "${GREEN}-----RUNNING TESTS-----${NC}\n"
 #./main.exe -b files/example1.txt >b
 #result=$(diff a b)
 #failed=0
-#c=1
+c=1
 
 # TEST 1
 
@@ -29,20 +29,35 @@ else
 fi
 ((c++))
 
-#for (( i = 0; i < 1; i++ )) # для одного флага
-#do
-#    cat ${arr[i]} test_1.txt >a
-#    ./s21_cat ${arr[i]} test_1.txt >b
-#    result=$(diff a b)
-#    if [ $? -eq 0 ]; then
-#	    printf " TEST #$c ${GREEN}PASSED${NC}\n"
-#    else
-#	    printf " TEST #$c ${RED}FAILED${NC}\n"
-#	    printf "$result"
-#	    ((failed++))
-#    fi
-#((c++))
-#done
+for (( i = 0; i < 1; i++ )) # для одного флага files/example.txt
+do
+    sort ${arr[i]} files/example.txt >a
+    ./main.exe ${arr[i]} files/example.txt >b
+    result=$(diff a b)
+    if [ $? -eq 0 ]; then
+	    printf " TEST #$c ${GREEN}PASSED${NC}\n"
+    else
+	    printf " TEST #$c ${RED}FAILED${NC}\n"
+	    printf "$result"
+	    ((failed++))
+    fi
+((c++))
+done
+
+for (( i = 0; i < 1; i++ )) # для одного флага files/example1.txt
+do
+    sort ${arr[i]} files/example1.txt >a
+    ./main.exe ${arr[i]} files/example1.txt >b
+    result=$(diff a b)
+    if [ $? -eq 0 ]; then
+	    printf " TEST #$c ${GREEN}PASSED${NC}\n"
+    else
+	    printf " TEST #$c ${RED}FAILED${NC}\n"
+	    printf "$result"
+	    ((failed++))
+    fi
+((c++))
+done
 
 
 
