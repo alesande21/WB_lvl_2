9b60516e4dbc6e4d01682636dd7505ea9b6288f1
alesande21 <via.rzv19@gmail.com>
2024-10-24 23:40:58 +0300

    upd 2.5

---
+++ Level_2/L2.5/internal/dictionary/dictionary.go
@@ -0,0 +1,25 @@
+package dictionary
+
+type Dictionary struct {
+	dic map[int64][]string
+}
+
+func NewDictionary() *Dictionary {
+	return &Dictionary{dic: make(map[int64][]string)}
+}
+
+func (d *Dictionary) CreateDictionary(str []string) map[int64][]string {
+
+	for _, word := range str {
+		key, lowerCase, err := d.getKeyForWord(word)
+		if err != nil {
+			continue
+		}
+
+	}
+}
+
+func (d *Dictionary) getKeyForWord(w string) (int64, string, error) {
+
+	return 0, "", nil
+}
--- Level_2/L2.5/main.go
+++ Level_2/L2.5/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+)
+
+func binary(s string) string {
+	res := ""
+	for _, c := range s {
+		res = fmt.Sprintf("%s%.8b", res, c)
+	}
+	return res
+}
 func main() {
+
+	//str := "пятак"
+	//str2 := "пятка"
+
+	var i int64
+
+	i |= 1 << 11
+	i |= 1 << 0
+	fmt.Printf("%b\n", i)
+	//fmt.Printf("%v\n", binary(str2))
 
 }
