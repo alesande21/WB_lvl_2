59820b3e9222063f2dfa8d54b378d7267aa244d8
alesande <alesande@student.21-school.ru>
2024-11-13 18:24:17 +0300

    upd 2.8

--- Level_2/L2.7/main.go
+++ Level_2/L2.7/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	out := make(chan interface{})
+	var once sync.Once
+	go func() {
+		for _, ch := range channels {
+			go func(in <-chan interface{}) {
+				select {
+				case <-in:
+					once.Do(func() { close(out) })
+				}
+			}(ch)
+		}
+	}()
+
+	return out
+}
+
 func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+	start := time.Now()
+	<-or(sig(1*time.Second),
+		sig(1*time.Second),
+		sig(1*time.Second))
+	fmt.Printf("fone after %v", time.Since(start))
 
 }
--- Level_2/L2.8/main.go
+++ Level_2/L2.8/main.go
@@ -1,91 +1,29 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"sync"
 	"time"
 )
 
 func or(channels ...<-chan interface{}) <-chan interface{} {
-	var wg sync.WaitGroup
-	wg.Add(len(channels))
 	out := make(chan interface{})
+	var once sync.Once
 	go func() {
-		defer close(out)
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-
-		for _, chIn := range channels {
-			go func(in <-chan interface{}, ctx context.Context) {
-				defer wg.Done()
-				for {
-					select {
-					case <-ctx.Done():
-						return
-					case <-in:
-						cancel()
-						close(out)
-					default:
-						continue
-					}
+		for _, ch := range channels {
+			go func(in <-chan interface{}) {
+				select {
+				case <-in:
+					once.Do(func() { close(out) })
 				}
-				fmt.Printf("exit\n")
-			}(chIn, ctx)
-
+			}(ch)
 		}
 	}()
-
-	fmt.Printf("AAAA\n")
 
 	return out
 }
 
 func main() {
-	//chs := make([]chan interface{}, 8)
-	//
-	//for i := range chs {
-	//	ch := make(chan interface{})
-	//	chs[i] = ch
-	//}
-	//
-	//chsIn := make([]<-chan interface{}, len(chs))
-	//for i := range chsIn {
-	//	chsIn[i] = chs[i]
-	//}
-	//
-	//chOut := or(chsIn...)
-	//
-	//go func() {
-	//	chs[0] <- "first"
-	//	chs[1] <- 2
-	//	close(chs[4])
-	//	chs[2] <- 4.12
-	//}()
-	//
-	////go func() { chs[0] <- "first" }()
-	////go func() { chs[1] <- 2 }()
-	////go func() {
-	////	chs[2] <- 4.12
-	////}()
-	////go func() { chs[3] <- [2]int{2, 6} }()
-	////go func() { chs[4] <- "for" }()
-	////go func() { chs[5] <- "A" }()
-	////go func() { chs[6] <- [4]int{0, 0, 0, 0} }()
-	////go func() { chs[7] <- "last" }()
-	//go func() {
-	//	time.Sleep(time.Second)
-	//	for _, ch := range chs {
-	//		_, ok := <-ch
-	//		if ok {
-	//			close(ch)
-	//		}
-	//	}
-	//}()
-	//
-	//for val := range chOut {
-	//	fmt.Printf("val: %v\n", val)
-	//}
 
 	sig := func(after time.Duration) <-chan interface{} {
 		c := make(chan interface{})
@@ -99,7 +37,7 @@ func main() {
 	<-or(
 		sig(2*time.Hour),
 		sig(5*time.Minute),
-		sig(1*time.Second),
+		sig(4*time.Second),
 		sig(1*time.Hour),
 		sig(1*time.Minute),
 	)
